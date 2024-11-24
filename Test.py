ee7bc092da84805d1b9a5ede7e237c656d38f934
souhaibothmani <souhaib.othmani@student.kdg.be>
2024-11-24 15:19:05 +0100

    AI model + esp32 sample test + test images + other AI models

---
+++ Test.py
@@ -0,0 +1,14 @@
+import requests
+
+# Define the endpoint and image file path
+url = "http://localhost:8080/uploadImage"
+file_path = "test-images/glass.png"
+
+# Send the POST request with the image
+with open(file_path, "rb") as file:
+    response = requests.post(url, files={"file": file})
+
+# Print the response as JSON
+print(response.json())
+
+
